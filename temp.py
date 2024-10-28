9fcde331143ec3159b0234d76c8c6ce0c9d1d8d7
Noah Pavlawk <noahpav@umich.edu>
2024-10-27 23:36:30 -0400

    Final Commit

--- temp.py
+++ temp.py
@@ -18,7 +18,7 @@ html_template = """
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Organization Home Page</title>
+    <title>Skyline Home Page</title>
     <link rel="stylesheet" href="css/reset.css" />
     <link rel="stylesheet" href="css/global.css" />
     <link rel="stylesheet" href="css/index.css" />
